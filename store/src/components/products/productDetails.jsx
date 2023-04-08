9726baa4812572da4fd1815b0bcdca7839ca3370
David J <98175896+jiangd22@users.noreply.github.com>
2023-04-07 20:45:41 -0500

    productDetails done

--- store/src/components/products/productDetails.jsx
+++ store/src/components/products/productDetails.jsx
@@ -24,23 +24,23 @@ export const ProductDetails = () => {
                 <nav aria-label="breadcrumb" className="p-2">
                     <ul className="breadcrumb mb-0">
                         <li className="breadcrumb-item"><a href="" className="text-primary text-decoration-none">Tasty snacks</a></li>
-                        <li className="breadcrumb-item text-muted" aria-current="page">Jif Peanut Butter, 40 ounce</li>
+                        <li className="breadcrumb-item text-body-tertiary" aria-current="page">Jif Peanut Butter, 40 ounce</li>
                     </ul>
                 </nav>
             </div>
 
             <div className="row bg-light py-4 mx-4">
-                <div className="col-3 p-0 ">
+                <div className="col-3 p-0">
                     <img src={product.imageUrl} className="img-fluid"></img>
                 </div>
                 <div className="col-8 p-0">
                     <p className="display-3 mb-0 fw-lighter">{product.name}</p>
                     <p className="fs-4 badge bg-primary ">${product.price}</p>
-                    <p className="fs-5">{product.description}</p>
+                    <p className="fs-5 fw-light text-body-tertiary">{product.description}</p>
                 </div>
             </div>
-            <ReviewList reviews={product.reviews} />
-            <ReviewForm addedReview={review=> addReview({reviews:[...product.reviews, review]})} />
+            {/* <ReviewList reviews={product.reviews} />
+            <ReviewForm addReview={review=> addReview({reviews:[...product.reviews, review]})} /> */}
         </div>
     </>
 
---
+++ store/src/components/products/reviewList.jsx
@@ -0,0 +1,24 @@
+// import { Rating } from "../common";
+
+// export const ReviewList = ({ reviews }) => {
+//     return <>
+
+//         <div className="row bg-light py-4 mx-4">
+//             <div className="col-12">
+//                 <h3 className="fs-4 fw-light">Product Reviews</h3>
+//                 <hr />
+//                 {reviews.map((review) => <div className="row mb-4">
+//                     <div className="col-2">
+//                         <Rating rating={review.rating} />
+//                     </div>
+//                     <div className="col-10">    
+//                         <p className="fs-5 fw-light">{review.comment}</p>
+//                         <p className="fs-6 fw-light text-body-tertiary">By {review.reviewer}</p>
+//                     </div>
+//                 </div>)}
+
+//             </div>
+//         </div>
+
+//     </>
+// }
