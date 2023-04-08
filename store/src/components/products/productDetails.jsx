fbd6b1aea41f315accd94df18b30c52cda0b2c1e
David J <98175896+jiangd22@users.noreply.github.com>
2023-04-07 20:33:19 -0500

    productDetails done, but still need to fintune the css

--- store/src/components/products/productDetails.jsx
+++ store/src/components/products/productDetails.jsx
@@ -4,27 +4,44 @@ import { ProductReview } from "../../models";
 import { ReviewForm } from "./reviewForm";
 import { ReviewList } from "./reviewList";
 
-export const ProductDetails = (props) => {
+export const ProductDetails = () => {
     const [product, setProduct] = useState({});
-    const [reviews, setReviews] = useState([]);
-    
+
     useEffect(() => {
-        getProductById(props.match.params.id).then((data) => {
-        setProduct(data);
-        });
-    }, [props.match.params.id]);
-    
+        getProductById(1).then((product) => setProduct(product));
+    }, []);
+
     const addReview = (review) => {
-        const newReview = new ProductReview(review);
-        setReviews([...reviews, newReview]);
+        const newReviews = [...product.reviews, review];
+        setProduct({ ...product, reviews: newReviews });
     };
-    
-    return (
-        <div>
-        <h2>{product.name}</h2>
-        <p>{product.description}</p>
-        <ReviewForm productId={product.id} addReview={addReview} />
-        <ReviewList reviews={reviews} />
+
+    if (!product.id){ return <>Loading...</>;}
+
+    return <>
+        <div className= "container">
+            <div className="row bg-body-secondary mb-4 py-2 mx-4">
+                <nav aria-label="breadcrumb" className="p-2">
+                    <ul className="breadcrumb mb-0">
+                        <li className="breadcrumb-item"><a href="" className="text-primary text-decoration-none">Tasty snacks</a></li>
+                        <li className="breadcrumb-item text-muted" aria-current="page">Jif Peanut Butter, 40 ounce</li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="row bg-light py-4 mx-4">
+                <div className="col-3 p-0 ">
+                    <img src={product.imageUrl} className="img-fluid"></img>
+                </div>
+                <div className="col-8 p-0">
+                    <p className="display-3 mb-0 fw-lighter">{product.name}</p>
+                    <p className="fs-4 badge bg-primary ">${product.price}</p>
+                    <p className="fs-5">{product.description}</p>
+                </div>
+            </div>
+            <ReviewList reviews={product.reviews} />
+            <ReviewForm addedReview={review=> addReview({reviews:[...product.reviews, review]})} />
         </div>
-    );
+    </>
+
 }
